946c3baae1a6f1428ce43f898bf4d9212ddc7ee2
rajnik-zibma <72140695+rajnik-zibma@users.noreply.github.com>
2021-04-05 11:25:13 +0530

    Update start_meeting_screen.dart

--- example/lib/start_meeting_screen.dart
+++ example/lib/start_meeting_screen.dart
@@ -1,13 +1,11 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:flutter_zoom_plugin/zoom_view.dart';
-import 'package:flutter_zoom_plugin/zoom_options.dart';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_zoom_plugin/zoom_options.dart';
+import 'package:flutter_zoom_plugin/zoom_view.dart';
 
 class StartMeetingWidget extends StatelessWidget {
-
   ZoomOptions zoomOptions;
   ZoomMeetingOptions meetingOptions;
 
@@ -16,9 +14,9 @@ class StartMeetingWidget extends StatelessWidget {
   String SDKKey = 'jJbXtCPPsbt3KpaPMqkCgL7t0zWoHwsKb98k';
   String SDKSecret = 'qmo84JZAg24dguzikPZX49QwajdhpxBiYtu3';
   String Username = 'viren@zibma.com';
-  String zoomAccessToken_ZAK = 'eyJ6bV9za20iOiJ6bV9vMm0iLCJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJjbGllbnQiLCJ1aWQiOiJhX05VV0JMTlNYNlBUOU1GbmVBYVh3IiwiaXNzIjoid2ViIiwic3R5IjoxMDAsIndjZCI6ImF3MSIsImNsdCI6MCwic3RrIjoiLUJuN1VORVYwejc3VEpYekRTbTJJMUtFbDMyWThPM1ViWGNWWHhkbjNnOC5CZ1lZY0d4R1RqbHBSbmQxTkRSSmFDdFVjR1IyZVZCS1VUMDlRREkzWWpreU1EQmlNVEZoWVRCa1l6TTVOREJtWlRCaFl6RXlZVEptTW1NeU1tRm1OV1F6Tm1WaVpHSXdZMlppWW1ObVlUQmpPV1ZqWkRka05XSXlaamtBRERORFFrRjFiMmxaVXpOelBRQURZWGN4QUFBQmVLQmpPUU1BRW5VQUFBQSIsImV4cCI6MTYxNzYwNTc2MCwiaWF0IjoxNjE3NTk4NTYwLCJhaWQiOiJONF9QdFZtVlRoYUJ1TUt4OFFVZEx3IiwiY2lkIjoiIn0.poVtGwe0JwK6QHpmfzb-F-wpQnlD5BJKqViiTbZ8xUs';
+  String zoomAccessToken_ZAK =
+      'eyJ6bV9za20iOiJ6bV9vMm0iLCJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJjbGllbnQiLCJ1aWQiOiJhX05VV0JMTlNYNlBUOU1GbmVBYVh3IiwiaXNzIjoid2ViIiwic3R5IjoxMDAsIndjZCI6ImF3MSIsImNsdCI6MCwic3RrIjoiLUJuN1VORVYwejc3VEpYekRTbTJJMUtFbDMyWThPM1ViWGNWWHhkbjNnOC5CZ1lZY0d4R1RqbHBSbmQxTkRSSmFDdFVjR1IyZVZCS1VUMDlRREkzWWpreU1EQmlNVEZoWVRCa1l6TTVOREJtWlRCaFl6RXlZVEptTW1NeU1tRm1OV1F6Tm1WaVpHSXdZMlppWW1ObVlUQmpPV1ZqWkRka05XSXlaamtBRERORFFrRjFiMmxaVXpOelBRQURZWGN4QUFBQmVLQmpPUU1BRW5VQUFBQSIsImV4cCI6MTYxNzYwNTc2MCwiaWF0IjoxNjE3NTk4NTYwLCJhaWQiOiJONF9QdFZtVlRoYUJ1TUt4OFFVZEx3IiwiY2lkIjoiIn0.poVtGwe0JwK6QHpmfzb-F-wpQnlD5BJKqViiTbZ8xUs';
   String displayName = 'Android';
-
 
   StartMeetingWidget({Key key, meetingId}) : super(key: key) {
     this.zoomOptions = new ZoomOptions(
@@ -27,17 +25,17 @@ class StartMeetingWidget extends StatelessWidget {
       appSecret: SDKSecret,
     );
     this.meetingOptions = new ZoomMeetingOptions(
-        userId: Username,
-        displayName: 'Rajnik',
-        meetingId: meetingId,
-        zoomAccessToken: zoomAccessToken_ZAK,
-        zoomToken: "<user_token>",
-        disableDialIn: "true",
-        disableDrive: "true",
-        disableInvite: "true",
-        disableShare: "true",
-        noAudio: "false",
-        noDisconnectAudio: "false",
+      userId: Username,
+      displayName: 'Rajnik',
+      meetingId: meetingId,
+      zoomAccessToken: zoomAccessToken_ZAK,
+      zoomToken: "<user_token>",
+      disableDialIn: "true",
+      disableDrive: "true",
+      disableInvite: "true",
+      disableShare: "true",
+      noAudio: "false",
+      noDisconnectAudio: "false",
     );
   }
 
@@ -45,7 +43,8 @@ class StartMeetingWidget extends StatelessWidget {
     var result = false;
 
     if (Platform.isAndroid)
-      result = status == "MEETING_STATUS_DISCONNECTING" || status == "MEETING_STATUS_FAILED";
+      result = status == "MEETING_STATUS_DISCONNECTING" ||
+          status == "MEETING_STATUS_FAILED";
     else
       result = status == "MEETING_STATUS_IDLE";
 
@@ -57,53 +56,47 @@ class StartMeetingWidget extends StatelessWidget {
     // Use the Todo to create the UI.
     return Scaffold(
       appBar: AppBar(
-          title: Text('Loading meeting '),
+        title: Text('Loading meeting '),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: ZoomView(onViewCreated: (controller) {
-
-          print("Created the view");
-
-          controller.initZoom(this.zoomOptions)
-              .then((results) {
-
-            print("initialised");
-            print(results);
-
-            if(results[0] == 0) {
-
-              controller.zoomStatusEvents.listen((status) {
-                print("Meeting Status Stream: " + status[0] + " - " + status[1]);
-                if (_isMeetingEnded(status[0])) {
-                  Navigator.pop(context);
-                  timer?.cancel();
-                }
-              });
-
-              print("listen on event channel");
-
-              controller.startMeeting(this.meetingOptions)
-                  .then((joinMeetingResult) {
-
-                timer = Timer.periodic(new Duration(seconds: 2), (timer) {
-                  controller.meetingStatus(this.meetingOptions.meetingId)
-                      .then((status) {
-                    print("Meeting Status Polling: " + status[0] + " - " + status[1]);
+          padding: EdgeInsets.all(16.0),
+          child: ZoomView(onViewCreated: (controller) {
+            print("Created the view");
+            controller.initZoom(this.zoomOptions).then((results) {
+              print("initialised");
+              print(results);
+              if (results[0] == 0) {
+                controller.zoomStatusEvents.listen((status) {
+                  print("Meeting Status Stream: " +
+                      status[0] +
+                      " - " +
+                      status[1]);
+                  if (_isMeetingEnded(status[0])) {
+                    Navigator.pop(context);
+                    timer?.cancel();
+                  }
+                });
+                print("listen on event channel");
+                controller
+                    .startMeeting(this.meetingOptions)
+                    .then((joinMeetingResult) {
+                  timer = Timer.periodic(new Duration(seconds: 2), (timer) {
+                    controller
+                        .meetingStatus(this.meetingOptions.meetingId)
+                        .then((status) {
+                      print("Meeting Status Polling: " +
+                          status[0] +
+                          " - " +
+                          status[1]);
+                    });
                   });
                 });
-
-              });
-            }
-
-          }).catchError((error) {
-
-            print("Error");
-            print(error);
-          });
-        })
-      ),
+              }
+            }).catchError((error) {
+              print("Error");
+              print(error);
+            });
+          })),
     );
   }
-
 }
